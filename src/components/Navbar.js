28254191c1080da3da87cac68627519dc7e91e58
qamarali205-github <qamarali205@gmail.com.com>
2023-03-15 15:46:10 +0530

    React-phone-Ecommerce

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,25 @@ export default class Navbar extends Component {
   render() {
     return (
       <Nav className="navbar navbar-expand-sm  navbar-dark px-sm-5">
+        <Link to='/'>
+          <img  src={logo} alt="store" className="navbar-brand"/>
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to='/'  className="nav-link">
+              Product
+            </Link>
+          </li>
+
+        </ul>
+        <Link to='/cart' className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+            <i className="fas fa-cart-plus" />
+            My Cart
+            </span>
+          </ButtonContainer>
+        </Link>
  
       </Nav>
     );
@@ -14,6 +33,12 @@ export default class Navbar extends Component {
 }
 
 const Nav = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+  color:var(--mainWhite) !important;
+  font-size:1.3rem;
+  text-transform:capitalize;
+}
 
 `;
 
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -11,7 +11,21 @@ export default class ProductList extends Component {
   render() {
     return (
       <React.Fragment>
-        START HACKING
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {value=>{
+                  return value.products.map(product =>{
+                    return <Product key={product.id} product={product} />
+                  })
+                }}
+              </ProductConsumer>
+              </div>
+          </div>
+        </div>
+        
       </React.Fragment>
     );
   }
