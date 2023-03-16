3d443654b5fcc8bc8dc7ffa8c5bb2ea6dd8d622b
qamarali205-github <qamarali205@gmail.com.com>
2023-03-16 12:21:40 +0530

    pass ho

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function EmptyCart() {
   return (
     <div className="container mt-5 empty-cart-msg">
-      <div className="row">
-        <div className="col-10 mx-auto text-center text-title text-capitalize">
-          <h1>your cart is currently empty</h1>
+      <div className="row empty-cart-msg">
+        <div className="col-10 mx-auto text-center text-title text-capitalize empty-cart-msg">
+          <h1 className="empty-cart-msg">your cart is currently empty</h1>
         </div>
       </div>
     </div>
